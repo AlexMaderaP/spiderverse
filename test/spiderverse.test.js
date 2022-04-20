a280783e335cc46d1af0c382f65bceeac1ef394c
AlexMaderaP <alejandro.maderap@gmail.com>
2022-04-20 18:42:20 -0500

    Agregando prueba fallida

---
+++ test/spiderverse.test.js
@@ -0,0 +1,9 @@
+describe("Test Suite Dummy Description", () => {
+    test('Case 1 Dummy', () => {
+
+        const resultOfSomething = 1 + 3;
+        expect(resultOfSomething).toBe(10);
+
+    });
+  })
+  
