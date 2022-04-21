6db9938ead5402246c236f58e6ecff8813c6a5fe
AlexMaderaP <alejandro.maderap@gmail.com>
2022-04-20 19:20:40 -0500

    Agregando metodo getInfo

--- test/spiderverse.test.js
+++ test/spiderverse.test.js
@@ -1,17 +1,31 @@
-const Spiderman = require('./../app/spiderman')
+const Spiderman = require("./../app/spiderman");
 
 describe("Unit Test for Spiderman Class", () => {
-    test('Case 1 Create an Spiderman Object', () => {
+  test("Case 1 Create an Spiderman Object", () => {
+    const andrewGarfield = new Spiderman(
+      "Amazing Spiderman",
+      32,
+      "Andrew Garfield",
+      3,
+      "Sony Pictures"
+    );
+    expect(andrewGarfield.name).toBe("Amazing Spiderman");
+    expect(andrewGarfield.age).toBe(32);
+    expect(andrewGarfield.actor).toBe("Andrew Garfield");
+    expect(andrewGarfield.movies).toBe(3);
+    expect(andrewGarfield.studio).toBe("Sony Pictures");
+  });
 
-        const andrewGarfield = new Spiderman("Amazing Spiderman",32,"Andrew Garfield",3,"Sony Pictures")
-        expect(andrewGarfield.name).toBe("Amazing Spiderman");
-        expect(andrewGarfield.age).toBe(32);
-        expect(andrewGarfield.actor).toBe("Andrew Garfield");
-        expect(andrewGarfield.movies).toBe(3);
-        expect(andrewGarfield.studio).toBe("Sony Pictures");
-
-    });
-  })
-  
-
-  
+  test("Case 2 Use the method getInfo()", () => {
+    const tomHolland = new Spiderman(
+      "Spiderman Marvel",
+      20,
+      "Tom Holland",
+      3,
+      "Marvel studio"
+    );
+    expect(tomHolland.getInfo()).toBe(
+      "Hey, I'm Tom Holland from Marvel studio"
+    );
+  });
+});
